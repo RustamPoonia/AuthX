9fed4a3cde367b73cdb49aba87b0fa62fdee178d
Rustam <rustampoonia@gmail.com>
2025-06-26 17:28:29 +0530

    � Initial commit: Resolve issues in  role-based auth

---
+++ src/main/java/com/authx/services/AdminService.java
@@ -0,0 +1,10 @@
+package com.authx.services;
+
+import java.util.List;
+
+import com.authx.userdto.AdminResponse;
+
+public interface AdminService {
+
+    List<AdminResponse> getAllUsers();
+}
---
+++ src/main/java/com/authx/userdto/Verifyemailrequest.java
@@ -0,0 +1,12 @@
+package com.authx.userdto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Verifyemailrequest {
+        private String otp;
+}
